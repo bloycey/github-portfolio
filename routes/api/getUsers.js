4c58973be3fd1da46e858ef20d4ccbcd481ce1b9
Andrew <bloyce1990@gmail.com>
2019-01-07 19:19:28 +1000

    Deleted redundant code

--- routes/api/getUsers.js
+++ routes/api/getUsers.js
@@ -15,26 +15,6 @@ async function getData(githubUsername) {
   return data;
 }
 
-// Function for getting individual profiles
-async function getUserData(githubUsername) {
-  let response = await fetch(
-    `https://api.github.com/users/${githubUsername}?client_id=${client_id}&client_secret=${client_secret}`
-  );
-  let data = await response.json();
-  return data;
-}
-
-//Functiona for getting usage data
-async function getUsageData() {
-  console.log("client id", client_id);
-  let response = await fetch(
-    `https://api.github.com/rate_limit?client_id=${client_id}&client_secret=${client_secret}`
-  );
-  let data = await response.json();
-  console.log("usage data", data);
-  return data;
-}
-
 // @route   GET api/getUsers
 // @desc    Gets usernames from github API based on search
 // @access  Public
@@ -46,29 +26,6 @@ router.post("/", (req, res) => {
   console.log(req.body);
   const username = req.body.githubName;
   getData(username).then(data => {
-    // console.log(data);
-    res.send({ response: data });
-  });
-});
-
-// Route for getting individual user data
-
-router.get("/", (req, res) => {
-  console.log("request recieved by server for user data " + req.query.username);
-  console.log(req.query.username);
-  const username = req.query.username;
-  getUserData(username).then(data => {
-    // console.log(data);
-    res.send({ response: data });
-  });
-});
-
-// Route for getting the rate limit
-
-router.get("/ratelimit", (req, res) => {
-  console.log("ratelimit request received");
-  getUsageData().then(data => {
-    // console.log(data);
     res.send({ response: data });
   });
 });
